504a9ed68a0fafa1cf7a6abb6700b1d0407f2d8f
沈云航 Yunhang Shen <shenyunhang01@gmail.com>
2024-04-07 09:46:05 +0800

    Update ape_deta_vitl_eva02_lsj1024.py

--- configs/D3_InstanceSegmentation/ape_deta/ape_deta_vitl_eva02_lsj1024.py
+++ configs/D3_InstanceSegmentation/ape_deta/ape_deta_vitl_eva02_lsj1024.py
@@ -6,14 +6,14 @@ from ...COCO_InstanceSegmentation.ape_deta.ape_deta_vitl_eva02_lsj1024_cp_12ep i
 )
 from ...common.data.d3_instance_lsj1024 import dataloader
 
-model.model_vision.dataset_prompts = ["expression"]
-model.model_vision.dataset_names = ["d3"]
+model.model_vision.dataset_prompts = ["expression",]
+model.model_vision.dataset_names = ["d3",]
 model.model_vision.dataset_metas = dataloader.train.dataset.names
 
 model.model_vision.num_classes = 256
 model.model_vision.criterion[0].num_classes = 256
 model.model_vision.select_box_nums_for_evaluation = 300
-model.model_vision.select_box_nums_for_evaluation_list = [300, 300]
+model.model_vision.select_box_nums_for_evaluation_list = [300,]
 
 model.model_vision.instance_on = True
 model.model_vision.semantic_on = False
